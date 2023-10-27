37b0c22d4900c22f48104a946e69001363f60e12
chenps3 <chenps3@qq.com>
2023-10-27 11:28:13 +0800

    feat():MergeModule.isMergeInProgress

---
+++ src/main/java/com/chenps3/git4j/modules/MergeModule.java
@@ -0,0 +1,15 @@
+package com.chenps3.git4j.modules;
+
+/**
+ * @Author chenguanhong
+ * @Date 2023/10/27
+ */
+public class MergeModule {
+
+    /**
+     * repo是否处于merge状态
+     */
+    public static boolean isMergeInProgress() {
+        return RefsModule.hash("MERGE_HEAD") != null;
+    }
+}
